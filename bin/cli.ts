c905997b81860e171d97f2a354c5895bb9cacc47
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2024-02-20 12:59:21 +0100

    🔧 fix: cli dev route glob

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,13 +1,12 @@
 #!/usr/bin/env bun
 
-import { $ } from 'bun'
+import { $, Glob } from 'bun'
 import type { RouteMeta } from '../src/routes'
 import { program } from 'commander'
 import { relative, resolve } from 'path'
-import { mkdir, readdir, rm } from 'fs/promises'
+import { mkdir, readdir, lstat, rm } from 'fs/promises'
 import { metaAnalysis } from '../src/routes'
 import { randomUUID } from 'crypto'
-import { glob } from 'glob'
 import { Galbe } from '../src'
 
 const ROOT = process.cwd()
@@ -19,22 +18,20 @@ const parseRoutes = async (routes?: string | string[]): Promise<{ path: string; 
   if (!routes) return []
   let files: { path: string; meta: RouteMeta }[] = []
   if (typeof routes === 'string') {
-    let filePaths = await glob(routes, { cwd: ROOT, withFileTypes: true, ignore: 'node_modules/**' })
-    for (const file of filePaths.map(f => ({ name: f.name, path: f.path, type: f.getType() }))) {
-      const path = `${file.path}/${file.name}`
-      if (file.type === 'File') files.push({ path, meta: await metaAnalysis(path) })
-      else if (file.type === 'Directory') {
+    for await (const path of new Glob(routes).scan({ cwd: ROOT, absolute: true, onlyFiles: false })) {
+      const isDir = (await lstat(path)).isDirectory()
+      if (isDir) {
         files = files.concat(
           await Promise.all(
             (
-              await readdir(`${file.path}/${file.name}`)
+              await readdir(path)
             ).map(async f => ({
-              path: `${file.path}/${f}`,
-              meta: await metaAnalysis(`${file.path}/${f}`)
+              path: f,
+              meta: await metaAnalysis(path)
             }))
           )
         )
-      }
+      } else files.push({ path, meta: await metaAnalysis(path) })
     }
   }
   if (Array.isArray(routes)) for (const r of routes) files = files.concat(await parseRoutes(r))
